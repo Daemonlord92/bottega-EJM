02a531cffa5721ab1f3f7eb390f7927fdc9f7b5f
3JM <zhintateo19924559@gmail.com>
2018-02-13 18:35:24 -0800

    cleaned up the blog a little bit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,7 @@ module ApplicationHelper
       (link_to "Login", new_user_session_path, class: style)
       
     else
+      link_to "Edit Profile", (edit_user_registration_path), class: style
       link_to "Logout", (destroy_user_session_path), method: :delete, class: style
     end
   end
