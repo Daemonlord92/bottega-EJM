34053e2e823d28105081452d2023e91e6aeabcb5
3JM <zhintateo19924559@gmail.com>
2018-02-16 07:19:33 -0800

    impeletment a nav helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,7 @@ module ApplicationHelper
   def login_helper style = ''
     if current_user.is_a?(GuestUser)
       (link_to "Register", new_user_registration_path, class: style) +
-      (link_to "Login", new_user_session_path, class: style)
-      
+      (link_to "Login", new_user_session_path, class: style)      
     else
       link_to "Edit Profile", (edit_user_registration_path), class: style
       link_to "Logout", (destroy_user_session_path), method: :delete, class: style
@@ -16,8 +15,24 @@ module ApplicationHelper
     end 
   end
   
-    
   def copyright_generator
     MjmViewTool::Renderer.copyright ' Matthew J Martin', 'All Rights Reserved'
   end
+
+  def nav_helper style, tag_type
+nav_links = <<NAV
+<#{tag_type}><a href="#{root_path}" class="#{style} #{active? root_path}">Home</a></#{tag_type}>
+<#{tag_type}><a href="#{about_path}" class="#{style} #{active? about_path}">About Me</a></#{tag_type}>
+<#{tag_type}><a href="#{contact_path}" class="#{style} #{active? contact_path}">Contact</a></#{tag_type}>
+<#{tag_type}><a href="#{blogs_path}" class="#{style} #{active? blogs_path}">Blog</a></#{tag_type}>
+<#{tag_type}><a href="#{portfolios_path}" class="#{style} #{active? portfolios_path}">Portfolios</a></#{tag_type}>
+NAV
+
+    nav_links.html_safe
+  end
+
+  def active? (path)
+    "active" if current_page? path
+  end
+
 end
